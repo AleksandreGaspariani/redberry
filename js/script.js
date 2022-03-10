8094d986c590e62f76756254d9f852cd2ee35880
AleksandreGaspariani <aleksandregasparyan@gmail.com>
2022-03-10 20:55:24 +0400

    removing unused function for convert data

--- js/script.js
+++ js/script.js
@@ -68,23 +68,6 @@ $('#submitApplication').on('click',()=>{
     sendPostRequest();
 });
 
-
-// data format 
-
-function formatDate(date) {
-    var d = new Date(date),
-        month = '' + (d.getMonth() + 1),
-        day = '' + d.getDate(),
-        year = d.getFullYear();
-
-    if (month.length < 2) 
-        month = '0' + month;
-    if (day.length < 2) 
-        day = '0' + day;
-
-    return [year, month, day].join('-');
-}
-
 // get all data for submit post.
 
 
